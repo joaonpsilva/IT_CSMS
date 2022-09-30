299b806dfa707df03a1bc6bb211cc18eb9281e24
joaonpsilva <joao.npsilva@ua.pt>
2022-09-30 17:29:04 +0100

    read id from command line

--- charge_station.py
+++ charge_station.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import websockets
 from ocpp.routing import after,on
+import sys
 
 from ocpp.v201 import call
 from ocpp.v201 import ChargePoint as cp
@@ -25,16 +26,19 @@ class ChargePoint(cp):
            print("Connected to central system.")
 
 
-async def main():
-   async with websockets.connect(
-       'ws://localhost:9000/CP_1',
-        subprotocols=['ocpp2.0.1']
-   ) as ws:
+async def main(id):
 
-       cp = ChargePoint('CP_1', ws)
+    cp_id = "CP_" + id
+    async with websockets.connect(
+        'ws://localhost:9000/' + cp_id,
+        
+            subprotocols=['ocpp2.0.1']
+    ) as ws:
 
-       await asyncio.gather(cp.start(), cp.send_boot_notification())
+        cp = ChargePoint(cp_id, ws)
+
+        await asyncio.gather(cp.start(), cp.send_boot_notification())
 
 
 if __name__ == '__main__':
-   asyncio.run(main())
+   asyncio.run(main(sys.argv[1]))
