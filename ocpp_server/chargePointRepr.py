343779cefaacdb5cc3fdd23f8e5b6dc7326d15f7
joaonpsilva <joao.npsilva@ua.pt>
2022-10-03 14:33:57 +0100

    denominations

---
+++ ocpp_server/chargePointRepr.py
@@ -0,0 +1,36 @@
+from ocpp.v201 import ChargePoint as cp
+from ocpp.v201 import call, call_result, enums, datatypes
+
+from ocpp.routing import on
+from datetime import datetime
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class ChargePoint(cp):
+    @on('BootNotification')
+    async def on_boot_notification(self, charging_station, reason, **kwargs):
+
+        return call_result.BootNotificationPayload(
+            current_time=datetime.utcnow().isoformat(),
+            interval=10,
+            status='Accepted'
+        )
+
+
+    async def getVariables(self, 
+        attributeType: enums.AttributeType,
+        component: datatypes.ComponentType,
+        variable: datatypes.VariableType):
+
+        request = call.GetVariablesPayload(get_variable_data=[{
+            'attributeType': attributeType,
+            'component': component,
+            'variable': variable
+        }])
+
+        response_get_variable = await self.call(request)
+    
+ 
