0f52e546686ba2169b2f66624d3b6625fb2c2f8a
joaonpsilva <joao.npsilva@ua.pt>
2022-10-03 10:29:22 +0100

    change funtion location

--- occp_server/csms.py
+++ occp_server/csms.py
@@ -21,29 +21,8 @@ class CSMS:
         self.connected_cp = {}
 
 
-    async def on_cp_connect(self,websocket, path):
-        """ For every new charge point that connects, create a ChargePoint
-        instance and start listening for messages.
-        """
-        try:
-            requested_protocols = websocket.request_headers[
-                'Sec-WebSocket-Protocol']
-        except KeyError:
-            logging.info("Client hasn't requested any Subprotocol. "
-                    "Closing Connection")
-        if websocket.subprotocol:
-            logging.info("Protocols Matched: %s", websocket.subprotocol)
-        else:
-            # In the websockets lib if no subprotocols are supported by the
-            # client and the server, it proceeds without a subprotocol,
-            # so we have to manually close the connection.
-            logging.warning('Protocols Mismatched | Expected Subprotocols: %s,'
-                            ' but client supports  %s | Closing connection',
-                            websocket.available_subprotocols,
-                            requested_protocols)
-            return await websocket.close()
-
-        charge_point_id = path.strip('/')
+    async def new_cp(self,websocket, charge_point_id):
+        
         cp = ChargePoint(charge_point_id, websocket)
 
         self.connected_cp[charge_point_id] = cp
@@ -78,7 +57,7 @@ class CSMS:
         self.ocpp_server = OCPP_Server()
         self.broker = CSMS_Rabbit_Handler()
 
-        await self.broker.connect(self.on_api_request)
+        await self.broker.connect(self.new_cp)
         await self.ocpp_server.start_server(self.on_cp_connect)
 
 
--- occp_server/ocpp_server.py
+++ occp_server/ocpp_server.py
@@ -6,15 +6,44 @@ logging.basicConfig(level=logging.INFO)
 
 class OCPP_Server:
 
+    def __init__(self):
+        self.callback_funtion = None
+
     async def start_server(self, callback_function):
+
+        self.callback_funtion = callback_function
+
         server = await websockets.serve(
-            callback_function,
+            self.on_cp_connect,
             '0.0.0.0',
             9000,
             subprotocols=['ocpp2.0.1']
         )
         logging.info("WebSocket Server Started")
 
-        
-
         await server.wait_closed()
+
+
+    async def on_cp_connect(self,websocket, path):
+        """ For every new charge point that connects, create a ChargePoint
+        instance and start listening for messages.
+        """
+        try:
+            requested_protocols = websocket.request_headers[
+                'Sec-WebSocket-Protocol']
+        except KeyError:
+            logging.info("Client hasn't requested any Subprotocol. "
+                    "Closing Connection")
+        if websocket.subprotocol:
+            logging.info("Protocols Matched: %s", websocket.subprotocol)
+        else:
+            # In the websockets lib if no subprotocols are supported by the
+            # client and the server, it proceeds without a subprotocol,
+            # so we have to manually close the connection.
+            logging.warning('Protocols Mismatched | Expected Subprotocols: %s,'
+                            ' but client supports  %s | Closing connection',
+                            websocket.available_subprotocols,
+                            requested_protocols)
+            return await websocket.close()
+
+        self.callback_funtion(websocket, path.strip('/'))
