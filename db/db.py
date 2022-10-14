7c96120b8050468b4128eb239a17d8ddc005b548
joaonpsilva <joao.npsilva@ua.pt>
2022-10-14 17:36:32 +0100

    create class structure. Establish connection to mysql database

--- db/db.py
+++ db/db.py
@@ -2,26 +2,38 @@ import asyncio
 from db_Rabbit_Handler import DB_Rabbit_Handler
 from aio_pika.abc import AbstractIncomingMessage
 import logging
+import mysql.connector
+
 logging.basicConfig(level=logging.INFO)
 
+class DataBase:
 
-async def on_api_request( message: AbstractIncomingMessage) -> None:
-    logging.info("REQUEST - %s", str(message))
+    def __init__(self):
+        self.broker = DB_Rabbit_Handler(self.on_api_request, self.on_log)
 
-async def on_log( message: AbstractIncomingMessage) -> None:
-    logging.info("STORE - %s", str(message))
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="password123"
+        )
+        self.cursor = self.db.cursor()
 
 
-async def main():
-    broker = DB_Rabbit_Handler(on_api_request, on_log)
+    async def on_api_request(self, message: AbstractIncomingMessage) -> None:
+        logging.info("REQUEST - %s", str(message))
 
-    await broker.connect()
+    async def on_log(self, message: AbstractIncomingMessage) -> None:
+        logging.info("STORE - %s", str(message))
+
+
+    async def run(self):
+        await self.broker.connect()
 
 
 
 if __name__ == '__main__':
     # Main part
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
 
-    loop.create_task(main())
+    loop.create_task(DataBase().run())
     loop.run_forever()
