806dc79cb6f5fb152815837b243ff29fa405bbcc
joaonpsilva <joao.npsilva@ua.pt>
2023-01-30 16:49:37 +0000

    kill processes when quitting

--- run_CSMS.sh
+++ run_CSMS.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
-python3 CSMS/db/db.py &
+#!/bin/bash
 python3 CSMS/api/api.py &
-python3 CSMS/ocpp_server/ocpp_server.py
+python3 CSMS/ocpp_server/ocpp_server.py &
+python3 CSMS/db/db.py &
+
+
+echo "Press 'q' to exit"
+while : ; do
+read -n 1 k <&1
+if [[ $k = q ]] ; then
+kill $(pgrep -f 'python3 CSMS/db/db.py')
+kill $(pgrep -f 'python3 CSMS/ocpp_server/ocpp_server.py')
+kill $(pgrep -f 'python3 CSMS/api/api.py')
+break
+fi
+done
+
+
+
